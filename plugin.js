70629fae2e45d5c5eff38a93b4c9f376f179df99
JuulArthur <juularthur92@gmail.com>
2015-10-14 19:01:56 +0200

    Change structure of plugin.js

--- plugin.js
+++ plugin.js
@@ -1,59 +1,72 @@
-'use strict';
+(function() {
+    'use strict';
 
-var actions    = require('./app/actions'),
-    filters    = require('./app/filters'),
-    settings   = require('./app/settings'),
-    controller = require('./app/controller'),
-    Plugin     = {};
+    var actions = require('./app/actions'),
+        filters = require('./app/filters'),
+        settings = require('./app/settings'),
+        controller = require('./app/controller'),
+        Plugin = {};
 
-//NodeBB list of Hooks: https://github.com/NodeBB/NodeBB/wiki/Hooks
-Plugin.hooks = {
-    actions: actions,
-    filters: filters,
-    statics: {
-        load: function (params, callback) {
-            var router       = params.router,
-                middleware   = params.middleware,
-                controllers  = params.controllers,
-                pluginUri    = '/admin/plugins/points',
-                apiUri       = '/api' + pluginUri,
-                renderAdmin  = function (req, res, next) {
-                    res.render(
-                        'admin/plugins/points', {}
-                    );
-                },
-                renderClient = function (req, res, next) {
-                    controller.getTopUsers(function (error, payload) {
-                        if (error) {
-                            return res.status(500).json(error);
-                        }
+    //NodeBB list of Hooks: https://github.com/NodeBB/NodeBB/wiki/Hooks
+    Plugin.hooks = {
+        actions: actions,
+        filters: filters,
+        statics: {
+            load: function (params, callback) {
+                var router = params.router,
+                    middleware = params.middleware,
+                    controllers = params.controllers,
+                    pluginUri = '/admin/plugins/points',
+                    apiUri = '/api' + pluginUri,
+                    renderAdmin = function (req, res, next) {
                         res.render(
-                            'client/points/overview', payload
+                            'admin/plugins/points', {}
                         );
-                    });
-                };
+                    },
+                    renderClient = function (req, res, next) {
+                        controller.getTopUsers(function (error, payload) {
+                            if (error) {
+                                return res.status(500).json(error);
+                            }
+                            res.render(
+                                'client/points/overview', payload
+                            );
+                        });
+                    };
 
-            router.get(pluginUri, middleware.admin.buildHeader, renderAdmin);
-            router.get(apiUri, renderAdmin);
+                router.get(pluginUri, middleware.admin.buildHeader, renderAdmin);
+                router.get(apiUri, renderAdmin);
 
-            //Client page
-            router.get('/points', middleware.buildHeader, renderClient);
-            router.get('/api/points', renderClient);
+                //Client page
+                router.get('/points', middleware.buildHeader, renderClient);
+                router.get('/api/points', renderClient);
 
-            settings.init(callback);
+                settings.init(callback);
+            }
+        },
+        admin: {
+            menu: function (custom_header, callback) {
+                custom_header.plugins.push({
+                    route: '/plugins/points',
+                    icon: 'fa-gamepad',
+                    name: 'Points'
+                });
+
+                callback(null, custom_header);
+            }
         }
-    },
-    admin: {
-        menu: function(custom_header, callback) {
-            custom_header.plugins.push({
-                route: '/plugins/points',
-                icon : 'fa-gamepad',
-                name : 'Points'
-            });
+    };
 
-            callback(null, custom_header);
-        }
-    }
-};
+    Plugin.admin = function (custom_header, callback) {
+        custom_header.plugins.push({
+            route: '/plugins/points',
+            icon: 'fa-gamepad',
+            name: 'Points'
+        });
 
-module.exports = Plugin;
+        callback(null, custom_header);
+    };
+
+    module.exports = Plugin;
+
+})();
