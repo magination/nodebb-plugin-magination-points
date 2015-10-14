42e8b52ebc69437231d304d937bdef3dd34cbf25
JuulArthur <juularthur92@gmail.com>
2015-10-14 17:42:40 +0200

    Change way files are exported

--- plugin.js
+++ plugin.js
@@ -1,47 +1,48 @@
-(function (Plugin) {
-    'use strict';
+'use strict';
 
-    var actions    = require('./app/actions'),
-        filters    = require('./app/filters'),
-        settings   = require('./app/settings'),
-        controller = require('./app/controller');
+var actions    = require('./app/actions'),
+    filters    = require('./app/filters'),
+    settings   = require('./app/settings'),
+    controller = require('./app/controller'),
+    Plugin     = {};
 
-    //NodeBB list of Hooks: https://github.com/NodeBB/NodeBB/wiki/Hooks
-    Plugin.hooks = {
-        actions: actions,
-        filters: filters,
-        statics: {
-            load: function (params, callback) {
-                var router       = params.router,
-                    middleware   = params.middleware,
-                    controllers  = params.controllers,
-                    pluginUri    = '/admin/plugins/points',
-                    apiUri       = '/api' + pluginUri,
-                    renderAdmin  = function (req, res, next) {
+//NodeBB list of Hooks: https://github.com/NodeBB/NodeBB/wiki/Hooks
+Plugin.hooks = {
+    actions: actions,
+    filters: filters,
+    statics: {
+        load: function (params, callback) {
+            var router       = params.router,
+                middleware   = params.middleware,
+                controllers  = params.controllers,
+                pluginUri    = '/admin/plugins/points',
+                apiUri       = '/api' + pluginUri,
+                renderAdmin  = function (req, res, next) {
+                    res.render(
+                        'admin/plugins/points', {}
+                    );
+                },
+                renderClient = function (req, res, next) {
+                    controller.getTopUsers(function (error, payload) {
+                        if (error) {
+                            return res.status(500).json(error);
+                        }
                         res.render(
-                            'admin/plugins/points', {}
+                            'client/points/overview', payload
                         );
-                    },
-                    renderClient = function (req, res, next) {
-                        controller.getTopUsers(function (error, payload) {
-                            if (error) {
-                                return res.status(500).json(error);
-                            }
-                            res.render(
-                                'client/points/overview', payload
-                            );
-                        });
-                    };
+                    });
+                };
 
-                router.get(pluginUri, middleware.admin.buildHeader, renderAdmin);
-                router.get(apiUri, renderAdmin);
+            router.get(pluginUri, middleware.admin.buildHeader, renderAdmin);
+            router.get(apiUri, renderAdmin);
 
-                //Client page
-                router.get('/points', middleware.buildHeader, renderClient);
-                router.get('/api/points', renderClient);
+            //Client page
+            router.get('/points', middleware.buildHeader, renderClient);
+            router.get('/api/points', renderClient);
 
-                settings.init(callback);
-            }
+            settings.init(callback);
         }
-    };
-})(module.exports);
+    }
+};
+
+module.exports = Plugin;
