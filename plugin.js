b8d2864d9fb01b08688f75279001a2eb42d8bca2
JuulArthur <juularthur92@gmail.com>
2015-10-14 21:27:41 +0200

    remove code that was not functioning

--- plugin.js
+++ plugin.js
@@ -1,61 +1,72 @@
 (function() {
     'use strict';
 
-    var actions = require('./app/actions'),
-        filters = require('./app/filters'),
-        settings = require('./app/settings'),
-        controller = require('./app/controller'),
-        Plugin = {};
+    var //actions = require('./app/actions'),
+    //filters = require('./app/filters'),
+    //settings = require('./app/settings'),
+    //controller = require('./app/controller'),
+        Plugin = {
+            init: function(data, callback) {
+                console.log("lets init");
+                function render(req, res, next) {
+                    res.render('admin/plugins/points' +
+                        '', {});
+                }
 
-    //NodeBB list of Hooks: https://github.com/NodeBB/NodeBB/wiki/Hooks
-    Plugin.hooks = {
-        actions: actions,
-        filters: filters,
-        statics: {
-            load: function (params, callback) {
-                var router = params.router,
-                    middleware = params.middleware,
-                    controllers = params.controllers,
-                    pluginUri = '/admin/plugins/points',
-                    apiUri = '/api' + pluginUri,
-                    renderAdmin = function (req, res, next) {
-                        res.render(
-                            'admin/plugins/points', {}
-                        );
-                    },
-                    renderClient = function (req, res, next) {
-                        controller.getTopUsers(function (error, payload) {
-                            if (error) {
-                                return res.status(500).json(error);
-                            }
-                            res.render(
-                                'client/points/overview', payload
-                            );
-                        });
-                    };
+                data.router.get('admin/plugins/points', data.middleware.admin.buildHeader, render);
+                data.router.get('api/admin/plugins/points', render);
 
-                router.get(pluginUri, middleware.admin.buildHeader, renderAdmin);
-                router.get(apiUri, renderAdmin);
+                callback();
+            },
+            /* actions: actions,
+             filters: filters,
+             statics: {
+             load: function (params, callback) {
+             var router = params.router,
+             middleware = params.middleware,
+             controllers = params.controllers,
+             pluginUri = '/admin/plugins/points',
+             apiUri = '/api' + pluginUri,
+             renderAdmin = function (req, res, next) {
+             res.render(
+             'admin/plugins/points', {}
+             );
+             },
+             renderClient = function (req, res, next) {
+             controller.getTopUsers(function (error, payload) {
+             if (error) {
+             return res.status(500).json(error);
+             }
+             res.render(
+             'client/points/overview', payload
+             );
+             });
+             };
 
-                //Client page
-                router.get('/points', middleware.buildHeader, renderClient);
-                router.get('/api/points', renderClient);
+             router.get(pluginUri, middleware.admin.buildHeader, renderAdmin);
+             router.get(apiUri, renderAdmin);
 
-                settings.init(callback);
+             //Client page
+             router.get('/points', middleware.buildHeader, renderClient);
+             router.get('/api/points', renderClient);
+
+             settings.init(callback);
+             }()
+             }, */
+
+            admin: {
+                menu: function (custom_header, callback) {
+                    console.log("points showing");
+                    custom_header.plugins.push({
+                        route: '/plugins/points',
+                        icon: 'fa-gamepad',
+                        name: 'Points'
+                    });
+
+                    callback(null, custom_header);
+                }
             }
-        },
-        admin: {
-            menu: function (custom_header, callback) {
-                console.log("points showing");
-                custom_header.plugins.push({
-                    route: '/plugins/points',
-                    icon: 'fa-gamepad',
-                    name: 'Points'
-                });
+        };
 
-                callback(null, custom_header);
-            }
-        }
-    };
-
+    module.exports = Plugin;
 })();
